d50d35d3c19cc764738843519b5c295a7e00940d
Devin G <engy100x@gmail.com>
2017-05-19 10:46:08 -0700

    finished up the TODOs

--- starter-code/server.js
+++ starter-code/server.js
@@ -8,11 +8,16 @@
 // Be sure to install that and save it as a dependency after you create your package.json.
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
 
 // TODO: Include all of the static resources as an argument to app.use()
+app.use(express.static('/public'));
 
 // TODO: Write a new route that will handle a request and send the new.html file back to the user
-
+app.get('/new', function(request, response) {
+  response.sendFile('/public/new.html', {root: '.'});
+})
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
@@ -24,4 +29,5 @@ app.post('/articles', bodyParser, function(request, response) {
 
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log('server is on localhost:' + PORT);
 });
